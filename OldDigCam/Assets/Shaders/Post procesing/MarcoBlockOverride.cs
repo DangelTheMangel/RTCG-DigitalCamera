5b48095c465ee415e4ade0ca9112628cf2e4d48f
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2024-11-20 21:10:45 +0100

    Initial commit

---
+++ OldDigCam/Assets/Shaders/Post procesing/MarcoBlockOverride.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/MarcoBlockOverride")]
+public sealed class MarcoBlockOverride : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    [Tooltip("Controls the intensity of the effect.")]
+    public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
+    public ClampedFloatParameter pixelationAmount = new ClampedFloatParameter(128f, 1f, 1080f);
+    public ClampedFloatParameter marcoBlock = new ClampedFloatParameter(128f, 1f, 1080f);
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && intensity.value > 0f;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Global Settings).
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    const string kShaderName = "Hidden/Shader/MarcoBlock";
+
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume MarcoBlockOverride is unable to load. To fix this, please edit the 'kShaderName' constant in MarcoBlockOverride.cs or change the name of your custom post process shader.");
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_Intensity", intensity.value);
+        m_Material.SetTexture("_MainTex", source);
+        m_Material.SetFloat("pixelationAmount", pixelationAmount.value);
+        m_Material.SetFloat("marcoBlock", marcoBlock.value);
+        HDUtils.DrawFullScreen(cmd, m_Material, destination, shaderPassId: 0);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
